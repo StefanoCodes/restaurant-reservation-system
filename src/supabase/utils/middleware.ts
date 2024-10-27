5433e5f9ba610a61f6cacca1c15182ddf48e78fb
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-10-27 16:24:11 +0300

    correctted bug in middleware

--- src/supabase/utils/middleware.ts
+++ src/supabase/utils/middleware.ts
@@ -42,7 +42,7 @@ export async function updateSession(request: NextRequest) {
 	) {
 		// no user, potentially respond by redirecting the user to the login page
 		const url = request.nextUrl.clone();
-		url.pathname = "/book-table";
+		url.pathname = "/login";
 		return NextResponse.redirect(url);
 	}
 
