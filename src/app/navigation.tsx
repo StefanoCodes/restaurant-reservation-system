f217a110da11b116961e58b10261fac7c9f5e6a9
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-10-27 15:58:37 +0300

    done

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -1,10 +1,14 @@
-import { inter } from "./layout";
 import { getUserDetails, getUserRole } from "@/lib/data";
 import { createClient } from "@/supabase/utils/server";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 import NavigationList from "./navigation-list";
-
+import { DM_Sans } from "next/font/google";
+const dmSans = DM_Sans({
+	subsets: ["latin"],
+	variable: "--font-dm-sans",
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 export default async function Navigation() {
 	const client = await createClient();
 	const { auth } = client;
@@ -19,7 +23,7 @@ export default async function Navigation() {
 			<div className="flex justify-between items-center max-w-7xl mx-auto py-4 px-4">
 				<Link href={`/`} className="cursor-pointer">
 					<h1
-						className={`text-xl font-semibold capitalize text-white ${inter.className}`}
+						className={`text-xl font-semibold capitalize text-white ${dmSans.className}`}
 					>
 						{user ? `Welcome ${user?.name}` : "Welcome to our resturant"}
 					</h1>
