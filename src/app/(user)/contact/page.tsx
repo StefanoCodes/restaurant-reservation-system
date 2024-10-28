b114e1bf42393113fe02004d9da108e6b4aa8562
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-10-28 23:43:21 +0300

    updated contact info page ui

--- src/app/(user)/contact/page.tsx
+++ src/app/(user)/contact/page.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { createClient } from "@/supabase/utils/server";
 import { getUserRole } from "@/lib/data";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 export default async function Page() {
 	const client = await createClient();
 	const {
@@ -16,14 +18,14 @@ export default async function Page() {
 	if (userRole !== "user") redirect("/");
 	return (
 		<main className="h-full w-full overflow-hidden">
-			<div className="w-full lg:grid lg:grid-cols-2 md:min-h-screen px-4 sm:px-0 relative">
-				<div className="flex items-center justify-center py-8 relative">
-					<div className="mx-auto grid max-w-xl gap-6 z-10">
+			<div className="w-full flex flex-col md:flex-row min-h-[100dvh] px-4 sm:px-0 relative">
+				<div className="flex items-center justify-center  py-8 md:py-0  flex-1">
+					<div className="mx-auto max-w-xl gap-6 z-10 ">
 						<div className="gap-2 flex flex-col">
 							<h1 className="text-4xl font-bold">Contact Information</h1>
 						</div>
 						<div className="flex flex-col gap-2">
-							<div className="flex flex-col gap-2">
+							<div className="flex flex-col gap-4">
 								<Label className="font-medium text-xl ">
 									Email:{" "}
 									<span className="font-normal text-muted-foreground">
@@ -42,20 +44,23 @@ export default async function Page() {
 										123 Main St, Trastevere, Italy
 									</span>
 								</Label>
-								<iframe
-									src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d43472.056165786926!2d31.230741205865964!3d30.04510286966119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583fad4c1e581b%3A0x8955d1d337fef452!2sRed%20Elephant%20Restaurant!5e0!3m2!1sen!2seg!4v1728903388357!5m2!1sen!2seg"
-									width="600"
-									height="450"
-									style={{ border: "0" }}
-									allowFullScreen
-									loading="lazy"
-									referrerPolicy="no-referrer-when-downgrade"
-								></iframe>
+								<Button
+									asChild
+									className="hover:scale-105 border-none transition-all duration-300  bg-gradient-to-tr from-orange-500 via-orange-600 to-orange-700 text-white"
+								>
+									<Link
+										href={
+											"https://www.google.com/maps/place/Red+Elephant+Restaurant/@30.0451029,31.2307412,17z/data=!3m1!4b1!4m6!3m5!1s0x14583fad4c1e581b:0x8955d1d337fef452!8m2!3d30.0451029!4d31.2333169!16s%2Fg%2F11c48yf53j"
+										}
+									>
+										View on Google Maps
+									</Link>
+								</Button>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div className="bg-muted lg:block z-4">
+				<div className=" flex-1">
 					<Image
 						src="/bg-restaurant.jpg"
 						alt="Image"
