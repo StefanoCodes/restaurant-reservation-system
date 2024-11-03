837cad852116bc2ce2f299e1754b9981196ff821
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-11-03 17:18:37 +0200

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,58 +3,56 @@ import { createServerClient } from "@supabase/ssr";
 import { NextResponse } from "next/server";
 
 export async function updateSession(request: NextRequest) {
-	let response = NextResponse.next();
+  const response = NextResponse.next();
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            response.cookies.set(name, value, options);
+          });
+        },
+      },
+    },
+  );
 
-	const supabase = createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-		{
-			cookies: {
-				getAll() {
-					return request.cookies.getAll();
-				},
-				setAll(cookiesToSet) {
-					cookiesToSet.forEach(({ name, value, options }) => {
-						request.cookies.set(name, value);
-						response.cookies.set(name, value, options);
-					});
-				},
-			},
-		}
-	);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-	const {
-		data: { user },
-	} = await supabase.auth.getUser();
+  // Redirect logic
+  const url = request.nextUrl.clone();
+  if (
+    !user &&
+    !url.pathname.startsWith("/login") &&
+    !url.pathname.startsWith("/register")
+  ) {
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
 
-	if (
-		!user &&
-		!request.nextUrl.pathname.startsWith("/login") &&
-		!request.nextUrl.pathname.startsWith("/register")
-	) {
-		const url = request.nextUrl.clone();
-		url.pathname = "/login";
-		return NextResponse.redirect(url);
-	}
-	if (
-		user &&
-		(request.nextUrl.pathname.startsWith("/login") ||
-			request.nextUrl.pathname.startsWith("/register"))
-	) {
-		const url = request.nextUrl.clone();
-		url.pathname = "/";
-		return NextResponse.redirect(url);
-	}
+  if (
+    user &&
+    (url.pathname.startsWith("/login") || url.pathname.startsWith("/register"))
+  ) {
+    url.pathname = "/";
+    return NextResponse.redirect(url);
+  }
 
-	return response;
+  return NextResponse.next(); // Ensure you return a new NextResponse
 }
 
 export async function middleware(request: NextRequest) {
-	return await updateSession(request);
+  return await updateSession(request);
 }
 
 export const config = {
-	matcher: [
-		"/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-	],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
