6bfee0e1c73dfd64f0bc0cd1a55b9c99da584ca5
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-11-03 13:35:15 +0200

    updated sidebar on mobile to close when a link is pressed and to avoid re-rendering

--- src/app/(admin)/admin/admin-sidebar-routes.tsx
+++ src/app/(admin)/admin/admin-sidebar-routes.tsx
@@ -6,17 +6,32 @@ import {
 } from "@/components/ui/sidebar";
 import { RoutesLink } from "@/lib/types";
 import { ADMIN_ROUTES } from "@/utils/constants";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import { useSidebar } from "@/components/ui/sidebar";
+import { useIsMobile } from "@/hooks/use-mobile";
 export default function AdminSidebarRoutes() {
   const pathname = usePathname();
+  const { toggleSidebar } = useSidebar();
+  const isMobile = useIsMobile();
   return (
     <SidebarMenu>
       {ADMIN_ROUTES.map((item: RoutesLink) => (
         <SidebarMenuItem key={item.name}>
           <SidebarMenuButton asChild>
-            <a
+            <Link
               href={item.path}
+              onClick={() => {
+                // only if we are on the mobile screen thought
+                if (isMobile) {
+                  // we want to check if he current path is the same as the clicked one we want to close the sidebar
+                  if (pathname === item.path) {
+                    toggleSidebar();
+                  } else {
+                    toggleSidebar();
+                  }
+                }
+              }}
               className={`${
                 pathname === item.path
                   ? "bg-orange-500/80 text-gray-50 hover:bg-orange-500/40 hover:text-gray-50"
@@ -25,7 +40,7 @@ export default function AdminSidebarRoutes() {
             >
               <item.icon className="h-4 w-4" />
               <span>{item.name}</span>
-            </a>
+            </Link>
           </SidebarMenuButton>
         </SidebarMenuItem>
       ))}
