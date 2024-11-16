25ece9fc86da8b203664ca8bdd1b05d2dec1e990
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-11-16 17:31:03 +0200

    removed missing promise

--- src/app/(admin)/admin/marketing/page.tsx
+++ src/app/(admin)/admin/marketing/page.tsx
@@ -1,20 +1,17 @@
 import { Suspense } from "react";
 import H1 from "../_components/h1";
 import { isAuthorizedAdmin } from "@/app/(auth)/auth";
-import { getTemplateName } from "@/lib/data/admin";
 import ButtonPendingLoader from "@/components/button-pending-loader";
 import TemplateList from "./_components/template-list";
 
 export default async function Page() {
   await isAuthorizedAdmin();
-  // pass down an unresolved promise to the template toggle
-  const templateName = getTemplateName();
   return (
     <div className="flex h-full w-full flex-col items-start justify-start gap-4">
       <H1>Marketing Configurations</H1>
       <Suspense fallback={<ButtonPendingLoader />}>
         <div className="w-full">
-          <TemplateList templateNamePromise={templateName} />
+          <TemplateList />
         </div>
       </Suspense>
     </div>
