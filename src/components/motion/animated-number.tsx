88c72da30abff7bdfd0bf1fe8691a1e4dc180cb2
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-11-18 13:32:47 +0200

    cleanup

--- src/components/motion/animated-number.tsx
+++ src/components/motion/animated-number.tsx
@@ -1,31 +1,31 @@
-"use client";
-import { cn } from "@/lib/utils";
-import { motion, SpringOptions, useSpring, useTransform } from "framer-motion";
-import { useEffect } from "react";
+// "use client";
+// import { cn } from "@/lib/utils";
+// import { motion, SpringOptions, useSpring, useTransform } from "framer-motion";
+// import { useEffect } from "react";
 
-type AnimatedNumber = {
-  value: number;
-  className?: string;
-  springOptions?: SpringOptions;
-};
+// type AnimatedNumber = {
+//   value: number;
+//   className?: string;
+//   springOptions?: SpringOptions;
+// };
 
-export function AnimatedNumber({
-  value,
-  className,
-  springOptions,
-}: AnimatedNumber) {
-  const spring = useSpring(value, springOptions);
-  const display = useTransform(spring, (current) =>
-    Math.round(current).toLocaleString(),
-  );
+// export function AnimatedNumber({
+//   value,
+//   className,
+//   springOptions,
+// }: AnimatedNumber) {
+//   const spring = useSpring(value, springOptions);
+//   const display = useTransform(spring, (current) =>
+//     Math.round(current).toLocaleString(),
+//   );
 
-  useEffect(() => {
-    spring.set(value);
-  }, [spring, value]);
+//   useEffect(() => {
+//     spring.set(value);
+//   }, [spring, value]);
 
-  return (
-    <motion.span className={cn("tabular-nums", className)}>
-      {display}
-    </motion.span>
-  );
-}
+//   return (
+//     <motion.span className={cn("tabular-nums", className)}>
+//       {display}
+//     </motion.span>
+//   );
+// }
