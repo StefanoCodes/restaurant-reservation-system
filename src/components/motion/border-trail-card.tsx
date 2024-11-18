321f3f5b9b4f31ae741dcb21a043f3e3af60b482
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-11-18 13:43:01 +0200

    clean

--- src/components/motion/border-trail-card.tsx
+++ src/components/motion/border-trail-card.tsx
@@ -1,19 +1,19 @@
-import { BorderTrail } from "@/components/motion/border-trail";
+// import { BorderTrail } from "@/components/motion/border-trail";
 
-export function BorderTrailCard1() {
-  return (
-    <div className="relative flex h-[200px] w-[300px] flex-col items-center justify-center rounded-md bg-zinc-200 px-5 py-2 dark:bg-zinc-900">
-      <div
-        className="flex h-full animate-pulse flex-col items-start justify-center space-y-2"
-        role="status"
-        aria-label="Loading..."
-      >
-        <div className="h-1 w-4 rounded-[4px] bg-zinc-600"></div>
-        <div className="h-1 w-10 rounded-[4px] bg-zinc-600"></div>
-        <div className="h-1 w-12 rounded-[4px] bg-zinc-600"></div>
-        <div className="h-1 w-12 rounded-[4px] bg-zinc-600"></div>
-        <div className="h-1 w-12 rounded-[4px] bg-zinc-600"></div>
-      </div>
-    </div>
-  );
-}
+// export function BorderTrailCard1() {
+//   return (
+//     <div className="relative flex h-[200px] w-[300px] flex-col items-center justify-center rounded-md bg-zinc-200 px-5 py-2 dark:bg-zinc-900">
+//       <div
+//         className="flex h-full animate-pulse flex-col items-start justify-center space-y-2"
+//         role="status"
+//         aria-label="Loading..."
+//       >
+//         <div className="h-1 w-4 rounded-[4px] bg-zinc-600"></div>
+//         <div className="h-1 w-10 rounded-[4px] bg-zinc-600"></div>
+//         <div className="h-1 w-12 rounded-[4px] bg-zinc-600"></div>
+//         <div className="h-1 w-12 rounded-[4px] bg-zinc-600"></div>
+//         <div className="h-1 w-12 rounded-[4px] bg-zinc-600"></div>
+//       </div>
+//     </div>
+//   );
+// }
